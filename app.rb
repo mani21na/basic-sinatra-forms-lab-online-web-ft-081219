25f2b1149605bc2afe3773e85033800276606d11
Hanna Lee <mani21na@gmail.com>
2019-11-01 16:44:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
   get "/newteam" do
@@ -6,13 +7,18 @@ class App < Sinatra::Base
   end 
   
   post '/team' do
-    @name = params[:name]
-    @coach = params[:coach]
-    @pg = params[:pg]
-    @sg = params[:sg]
-    @pf = params[:pf]
-    @sf = params[:sf]
-    @c = params[:c]
-    erb :team
+    binding.pry
+    if !params.blank
+      @name = params[:name]
+      @coach = params[:coach]
+      @pg = params[:pg]
+      @sg = params[:sg]
+      @pf = params[:pf]
+      @sf = params[:sf]
+      @c = params[:c]
+      erb :team
+    else
+      erb :newteam
+    end
   end  
 end
