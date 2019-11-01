c1fbdccf0c07dc1dfa7c99c21dc9c3155d0c897b
Hanna Lee <mani21na@gmail.com>
2019-11-01 00:14:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,8 +5,14 @@ class App < Sinatra::Base
     erb :newteam
   end 
   
-  post "/newteam" do
-    @team = params
+  post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
     erb :team
-  
+  end  
 end
